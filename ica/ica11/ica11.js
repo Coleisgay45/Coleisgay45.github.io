f9ffaf64e897f5070ce76cfe0ec89b7ec2a74b7d
Coleisgay45 <63007624+Coleisgay45@users.noreply.github.com>
2026-03-25 12:24:26 -0600

    ica11-done

--- ica/ica11/ica11.js
+++ ica/ica11/ica11.js
@@ -32,7 +32,10 @@ let events = [
 // Partial return random string function
 
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+  let randomCharacter = randomValueFromArray(characters);
+  let randomPlace = randomValueFromArray(places);
+  let randomEvent = randomValueFromArray(events);
+  let storyText = "It was 94 Fahrenheit outside, so " + randomCharacter + " went for a walk. When they got to " + randomPlace + ", they stared in horror for a few moments, then " + randomEvent + ". Bob saw the whole thing, but was not surprised — " + randomCharacter + " weighs 300 pounds, and it was a hot day."
 
   return storyText;
 }
@@ -42,16 +45,21 @@ function returnRandomStoryString() {
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString();
+
   if (customName.value !== "") {
     const name = customName.value;
+    newStory = newStory.replace("Bob", name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature = Math.round(94);
+    const weight = Math.round(300 / 14) + " stone";
+    const temperature = Math.round((94 - 32) * 5 / 9) + " Celsius";
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
   }
 
-  // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
